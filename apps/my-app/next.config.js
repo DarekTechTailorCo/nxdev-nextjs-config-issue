70536694b9b5762e37d0dd3650308b042400a8eb
dariuszwietecha <darek@techtailor.co>
2020-10-23 14:57:22 +0200

    nxdev-nextjs-config-issue

---
+++ apps/my-app/next.config.js
@@ -0,0 +1,8 @@
+const { createSecureHeaders } = require('next-secure-headers');
+
+module.exports = {
+  async headers() {
+    return [{ source: '/(.*)', headers: createSecureHeaders() }];
+  },
+  poweredByHeader: true,
+};
